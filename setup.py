78cf0568e81d5699c40f153e70f017e1d50cf5aa
VaheC <vcharchian@gmail.com>
2024-05-08 22:23:47 +0400

    setup.py is set

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="vehicle_loan_default",
+    version="0.0.0",
+    author="VaheCharchyan",
+    author_email="vcharchian@gmail.com",
+    packages=find_packages()
+)
