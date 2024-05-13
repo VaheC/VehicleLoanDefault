61a2d08858d85a5815397f84d1aff2906d2265cb
VaheC <vcharchian@gmail.com>
2024-05-13 15:37:21 +0400

    data validation part added

--- vehicle_loan_default/pipeline/training_pipeline.py
+++ vehicle_loan_default/pipeline/training_pipeline.py
@@ -3,11 +3,13 @@ from vehicle_loan_default.exception import VehicleLoanException
 from vehicle_loan_default.logger import logging
 
 from vehicle_loan_default.components.data_ingestion import DataIngestion
-
-from vehicle_loan_default.entity.config_entity import (DataIngestionConfig)
+from vehicle_loan_default.components.data_validation import DataValidation
+from vehicle_loan_default.entity.config_entity import (DataIngestionConfig,
+                                                       DataValidationConfig)
                                           
 
-from vehicle_loan_default.entity.artifact_entity import (DataIngestionArtifact)
+from vehicle_loan_default.entity.artifact_entity import (DataIngestionArtifact,
+                                                         DataValidationArtifact)
 
 
 
@@ -33,6 +35,30 @@ class TrainPipeline:
         except Exception as e:
             raise VehicleLoanException(e, sys) from e
         
+    def start_data_validation(self, data_ingestion_artifact: DataIngestionArtifact) -> DataValidationArtifact:
+        """
+        This method of TrainPipeline class is responsible for starting data validation component
+        """
+        logging.info("Entered the start_data_validation method of TrainPipeline class")
+
+        try:
+            data_validation = DataValidation(data_ingestion_artifact=data_ingestion_artifact,
+                                             data_validation_config=self.data_validation_config
+                                             )
+
+            data_validation_artifact = data_validation.initiate_data_validation()
+
+            logging.info("Performed the data validation operation")
+
+            logging.info(
+                "Exited the start_data_validation method of TrainPipeline class"
+            )
+
+            return data_validation_artifact
+
+        except Exception as e:
+            raise VehicleLoanException(e, sys) from e
+        
     
     def run_pipeline(self, ) -> None:
         """
@@ -40,5 +66,6 @@ class TrainPipeline:
         """
         try:
             data_ingestion_artifact = self.start_data_ingestion()
+            data_validation_artifact = self.start_data_validation(data_ingestion_artifact=data_ingestion_artifact)
         except Exception as e:
             raise VehicleLoanException(e, sys)
